4dad6bc3ec625fd18114c59b9dcb115d395b9184
B. de Grandmaison <bdegrandmaison@hotmail.fr>
2024-07-28 16:50:08 +0200

    fixed issue with modes

--- src/mocks/initializeWorker.ts
+++ src/mocks/initializeWorker.ts
@@ -3,7 +3,10 @@ import { worker } from "./browser";
 export const initializeWorker = async () => {
   await worker.start({
     serviceWorker: {
-      url: "/ecotravel/mockServiceWorker.js",
+      url:
+        import.meta.env.MODE === "production"
+          ? "/ecotravel/service-worker.js"
+          : "mockServiceWorker.js",
     },
     onUnhandledRequest: "bypass",
   });
